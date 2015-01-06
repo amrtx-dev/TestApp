cf517ea1bb0f95e95b8f2022663714b682f034cb
amrtx-dev <ad_michael@hotmail.com>
2015-01-05 22:55:48 -0600

    Initial Structure

---
+++ Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Layouts/Default.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TestApp.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div>
+        <asp:Label runat="server" ID="lblCategory" Text="Category:"></asp:Label>
+
+        <asp:DropDownList runat="server" ID="selCategory">
+            <asp:ListItem Text="Category1" Value="1"></asp:ListItem>
+            <asp:ListItem Text="Category2" Value="2"></asp:ListItem>
+        </asp:DropDownList>
+
+        <asp:Button runat="server" ID="btnSave" UseSubmitBehavior="false" 
+            Text="Click Me!" onclick="btnSave_Click" />
+    </div>
+
+
+
+    <asp:ScriptManager runat="server" ID="ScriptManager1">
+    </asp:ScriptManager>
+
+    <h2>List of Stuff</h2>
+    <hr />
+
+    <asp:UpdatePanel runat="server" ID="UpdatePanel1">
+        <Triggers>
+            <asp:AsyncPostBackTrigger ControlID="btnSave" EventName="Click" />
+        </Triggers>
+        <ContentTemplate>
+    
+            <asp:ListBox runat="server" ID="lstStuff">
+            </asp:ListBox>
+    
+        </ContentTemplate>
+    </asp:UpdatePanel>
+
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var categoryValue = GetSelectedCategory().ToInteger();
+                PopulateList(categoryValue);
+            }
+            catch
+            {
+                throw new ApplicationException("Error");
+            }
+
+        }
+
+        private void PopulateList(int categoryValue)
+        {
+            var items = new List<ListItem>();
+            switch (categoryValue)
+            {
+                case 1:
+                    items.Add(new ListItem("Cat1:Item1", "1"));
+                    items.Add(new ListItem("Cat1:Item2", "2"));
+                    break;
+                case 2:
+                    items.Add(new ListItem("Cat2:Item1", "1"));
+                    items.Add(new ListItem("Cat2:Item2", "2"));
+                    items.Add(new ListItem("Cat2:Item3", "3"));
+                    break;
+                default:
+                    throw new ApplicationException("Kaboom");
+            }
+            lstStuff.Items.Clear();
+            lstStuff.Items.AddRange(items.ToArray());
+        }
+
+
+        private string GetSelectedCategory()
+        {
+            var result = string.Empty;
+            var selectedItem = selCategory.SelectedItem;
+            if (selectedItem != null)
+            {
+                result = selectedItem.Value;
+            }
+            return result;
+        }
+
+ 
+ 
+    }
+
+
+    public static class Helpers
+    {
+
+        public static int ToInteger(this string value)
+        {
+            return Convert.ToInt32(value);
+        }     
+    }
+}
